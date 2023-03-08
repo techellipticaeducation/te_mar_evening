14bd9a1674bba7361b807a061eddaedc63533d31
vaisinghequinix <vaisingh@equinix.com>
2023-03-08 18:22:08 +0530

    updates

--- src/te_mar_evening/Test2.java
+++ src/te_mar_evening/Test2.java
@@ -1,7 +1,39 @@
 package te_mar_evening;
 
 public class Test2 {
-public static void main(String[] args) {
-	System.out.println("Hello World . this is test2");
-}
+	public static void main(String[] args) {
+		System.out.println("Hello World . this is test2");
+		
+		
+		// C:\Users\Vaibhav\git\te_mar_evening\src\test1
+		// package 
+		// any class - first line - package 
+		//test2\test3
+		// test2.test3
+		
+		
+		// variables
+		// Keywords
+		
+		
+		// Keyword -  predefined words in language - pre logic
+		// small letter 
+		
+		
+		// Variable = vary + able = 
+		
+		int i = 10;
+		i = 12;
+
+		// constant = final keyword
+		// Rule 2 = variable name cannot be keyword name
+		int _p = 19;
+		int $p = 19;
+		int Abcdkjhds = 19;
+		
+		
+		
+		// Datatype concept
+
+	}
 }
---
+++ src/test2/test3/Test1.java
@@ -0,0 +1,5 @@
+package test2.test3;
+
+public class Test1 {
+
+}
